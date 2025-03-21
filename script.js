b5f942f9dcf19ecaaba3fbb9269bc0f54e39d2ef
Thiago <dmthiagoribeiro@gmail.com>
2025-03-21 18:49:08 -0300

    um dos commit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function calculando(){
     const numero1 = document.getElementById("num1");
-    let valor1 = numero1.Value
+    let valor1 = numero1.value
     const numero2 = document.getElementById("num2");
-    let valor2 = numero2.Value
+    let valor2 = numero2.value
+    
+    if (valor1==valor2){
+        alert("os numeros não podem ser igual")
+    }
     if (valor2 >= valor1){
         alert(" sucesso ! O segundo numero e maior que o primeiro");
         } 
     else {
-        alert(" hFracasso ! o primeiro numero e maior que o segundo ");
+        alert(" Fracasso ! o primeiro numero e maior que o segundo ");
     }
 }
